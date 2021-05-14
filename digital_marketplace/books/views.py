13ee22b0e256b45e198bcd44b8c82f8998c18814
bhargav <sriram.bhargav76@gmail.com>
2021-05-14 10:21:17 -0400

    Final commit

---
+++ digital_marketplace/books/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Chapter, Exercise
+from django.http import Http404
+from shopping_cart.models import Order, OrderItem 
+
+OWNED='owned'
+IN_CART='in_cart'
+NOT_IN_CART='not_in_cart'
+
+
+def check_book_relationship(request,book):
+    print((request.user.userlibrary.books.all()))
+    print(book)
+    if book in request.user.userlibrary.books.all():
+        pritn(OWNED)
+        return OWNED
+    order_qs = Order.objects.filter(user=request.user)
+    print(request.user)
+    if order_qs.exists():
+        order=order_qs[0]
+        order_item_qs = OrderItem.objects.filter(book=book)
+        if order_item_qs.exists():
+            order_item = order_item_qs[0]
+            if order_item in order.items.all():
+                print(IN_CART)
+                return IN_CART
+    print((request.user.userlibrary.books.all()))
+    print(NOT_IN_CART)
+    return NOT_IN_CART
+
+def book_list(request):
+    queryset = Book.objects.all()
+    context = {
+        'queryset': queryset
+    }
+    return render(request, 'book_list.html', context)
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    book_status = check_book_relationship(request, book)
+    context={
+        'book':book,
+        'book_status': book_status
+    }
+    return render(request, 'book_detail.html', context)
+
+def chapter_detail(request, book_slug, chapter_number):
+    chapter_qs = Chapter.objects.filter(book__slug=book_slug).filter(chapter_number=chapter_number)
+    chapter = chapter_qs[0]
+    book_status = check_book_relationship(request, chapter.book)
+    if chapter_qs.exists():
+        context={
+            'chapter':chapter,
+            'book_status': book_status
+        }
+        return render(request, 'chapter_detail.html', context)
+    return Http404
+
+def exercise_detail(request, book_slug, chapter_number, exercise_number):
+    exercise_qs = Exercise.objects.filter(chapter__book__slug=book_slug).filter(chapter__chapter_number=chapter_number).filter(exercise_number=exercise_number)
+    exercise = exercise_qs[0]
+    book_status = check_book_relationship(request, exercise.chapter.book)
+    if exercise_qs.exists():
+        context={
+            'exercise': exercise,
+            'book_status': book_status
+        }
+        return render(request, 'exercise_detail.html', context)
+    return Http404
